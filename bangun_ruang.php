1f52a0dc6fa131473224e7f8127c37366240d4e2
ardiansyah-sweng <ardiansyah.2019@outlook.com>
2020-09-22 13:20:02 +0700

    Update dokumentasi fungsi keliling lingkaran

--- bangun_ruang.php
+++ bangun_ruang.php
@@ -1,16 +1,46 @@
-<?
-class Matematika
+<?php
+
+class BangunRuang
 {
     private $phi = 3.14;
 
     /**
      * Menghitung nilai kuadrat pangkat dua
-     * pangkat = nilai^2
+     * pangkat = nilai^pangkat
      */
     function kuadrat($nilai)
     {
         $pangkat = 2;
         return pow($nilai, $pangkat);
     }
-    
+
+    /**
+     * Menghitung Luas Lingkaran
+     * Rumus: 0.25 * 3.14 * diameter kuadrat
+     */
+    function luasLingkaran($diameter)
+    {
+        $seperempat = 0.25;
+        return $seperempat * $this->phi * $this->kuadrat($diameter);
+    }
+
+    /**
+     * Menghitung Keliling Lingkaran
+     * Rumus: 3.24 x 2 x r
+     */
+    function kelilingLingkaran($jari_jari)
+    {
+        $dua = 2;
+        return $this->phi * $dua * $jari_jari;
+    }
+
+    function luasKubus()
+    {
+
+    }
 }
+
+$bangunRuang = new BangunRuang();
+echo 'Luas lingkaran: '.$bangunRuang->luasLingkaran(30);
+echo '<br>';
+echo 'Keliling lingkaran: '.$bangunRuang->kelilingLingkaran(8);
