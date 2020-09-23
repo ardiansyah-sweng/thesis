2317761851d9dc1b018c73a9a126cd41e6d61e37
dmonh3h3 <adhymasfs@gmail.com>
2020-09-23 20:35:22 +0700

    add function luas permukaan dan volume Limas Segi3

--- bangun_ruang.php
+++ bangun_ruang.php
@@ -43,11 +43,31 @@ class BangunRuang
         $enam_sisi = 6;
         return $enam_sisi * $this->kuadrat($sisi);
     }
+
+    // add
+    private $la = 30; // Var $la = Luas Alas 
+
+    public function volumeLimasSegiTiga()
+    {
+        $t = 10; // Var $t = tinggi
+        $v =  (1 / 3 * $this->la * $t);  // Rumus Volume Limas Segi Tiga
+        return $v;
+    }
+    public function luaspermukaanLimasSegiTiga()
+    {
+        $lsl = 30; // var $lsl = luas selubung limas
+        $l_tot = $this->la + $lsl;  // Rumus Luas Permukaan Limas Segi Tiga
+        return $l_tot;
+    }
 }
 
 $bangunRuang = new BangunRuang();
-echo 'Luas lingkaran: '.$bangunRuang->luasLingkaran(30);
+echo 'Luas lingkaran: ' . $bangunRuang->luasLingkaran(30);
 echo '<br>';
-echo 'Keliling lingkaran: '.$bangunRuang->kelilingLingkaran(8);
+echo 'Keliling lingkaran: ' . $bangunRuang->kelilingLingkaran(8);
 echo '<br>';
-echo 'Luas permukaan kubus: '.$bangunRuang->luasPermukaanKubus(13);
+echo 'Luas permukaan kubus: ' . $bangunRuang->luasPermukaanKubus(13);
+echo '<br>';
+echo 'Volume Limas Segi 3: ' . $bangunRuang->volumeLimasSegiTiga();
+echo '<br>';
+echo 'Luas permukaan Limas Segi 3: ' . $bangunRuang->luaspermukaanLimasSegiTiga();
