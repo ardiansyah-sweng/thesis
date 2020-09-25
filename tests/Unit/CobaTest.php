44e5347b8f2a84148e4e3b84bec9bdc120d36afb
ardiansyah-sweng <ardiansyah.2019@outlook.com>
2020-09-25 16:29:40 +0700

    Add unit test kuadrat

--- tests/Unit/CobaTest.php
+++ tests/Unit/CobaTest.php
@@ -3,18 +3,14 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
-use App\Box;
+use App\BangunRuang;
 
 class CobaTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    /** @test */
+    public function nilai_kuadrat_harus_pangkat_dua()
     {
-        $box = new Box();
-        $this->assertEquals(8, $box->kuadrat(3));
+        $pangkatDua = new BangunRuang();
+        $this->assertEquals(9, $pangkatDua->kuadrat(3));
     }
 }
