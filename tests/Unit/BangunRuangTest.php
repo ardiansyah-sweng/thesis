c52d4b81bfa454ef9a5dbbb6e672204a13ce004e
dmonh3h3 <adhymasfs@gmail.com>
2020-09-28 13:26:42 +0700

    tes Unit Function Volume dan Luas Limas Segitiga

--- tests/Unit/BangunRuangTest.php
+++ tests/Unit/BangunRuangTest.php
@@ -159,6 +159,7 @@ class BangunRuangTest extends TestCase
         $actualValue = $volumeKubus->volumeKubus($sisi);
         $this->assertEquals($expectedValue, $actualValue);
     }
+    /** @test */
     public function tes_Volume_Limas_Segitiga()
     {
         $volumeLimasSegiTiga = new BangunRuang();
@@ -168,6 +169,7 @@ class BangunRuangTest extends TestCase
         $actualValue = $volumeLimasSegiTiga->volumeLimasSegiTiga($luasAlas, $tinggi);
         $this->assertEquals($expectedValue, $actualValue);
     }
+    /** @test */
     public function tes_Luas_Permukaan_Limas_Segitiga()
     {
         $volumeLimasSegiTiga = new BangunRuang();
