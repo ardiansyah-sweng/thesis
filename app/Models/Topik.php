7bbcb8d902a151b774fefab5a21b1e9e85d8c789
muhnashir <muhammadnashir9@gmail.com>
2020-10-08 13:05:38 +0700

    Menambahkan validasi form dan menampilkan topik pada form input

---
+++ app/Models/Topik.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Topik extends Model
+{
+    protected $table = 'topik_bidang';
+}
