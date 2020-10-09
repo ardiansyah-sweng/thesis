8ddeadaed2760dcc8af440c4a87e92d81dcb5ac7
wais46 <w.alqorni240@gmail.com>
2020-10-09 15:28:07 +0800

    Update Query Input form topik_tugas_akhir k dtabse

--- app/Http/Controllers/TopikController.php
+++ app/Http/Controllers/TopikController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Topik;
+use App\Models\TopikTugasAkhir;
 use Illuminate\Http\Request;
 
 class TopikController extends Controller
 {
     public function index(){
-        $topik = Topik::orderBy('topik_bidang','asc')->get();
+       $topik = Topik::orderBy('topik_bidang','asc')->get();
         return view('topik',compact('topik'));
     }
 
@@ -19,7 +20,12 @@ class TopikController extends Controller
             'deskripsi' => 'required|min:5',
         ]);
         
-        dd($request);
-        //habis ini query ke database
+        $store = new TopikTugasAkhir;
+        $store->nipy_fk_nipy = "001";
+        $store->topik_bidang_fk_id = "2";
+        $store->judul_topik = $request->judul;
+        $store->deskripsi = $request->deskripsi;
+        $store->save();
+        
     }
 }
