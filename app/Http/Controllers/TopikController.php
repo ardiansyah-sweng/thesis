2ee1e89c77db055cc63c486c10f88a5669e7c8c6
ardiansyah-sweng <ardiansyah.2019@outlook.com>
2020-10-14 13:09:39 +0700

    Add view, controller, route untuk view all topik

--- app/Http/Controllers/TopikController.php
+++ app/Http/Controllers/TopikController.php
@@ -6,22 +6,42 @@ use App\Models\Topik;
 use App\Models\TopikTugasAkhir;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+
 
 class TopikController extends Controller
 {
-    public function index(){
-       $topik = Topik::orderBy('topik_bidang','asc')->get();
-        return view('topik',compact('topik'));
+    public function index()
+    {
+        $topik = Topik::orderBy('topik_bidang', 'asc')->get();
+        return view('topik', compact('topik'));
     }
 
-    public function store(Request $request){     
+    # Query seluruh topik tugas akhir
+    public function all()
+    {
+        $nipy = Session::get('nipy');
+        
+        $allTopikTA = DB::select('SELECT topik_bidang.topik_bidang, dosen.nama, topik.judul_topik, topik.deskripsi, topik.status, topik.nim_terpilih_fk, COUNT(ambil.topik_tugas_akhir_id) AS jumlah_pendaftar
+            FROM topik_tugas_akhir topik
+            JOIN dosen ON dosen.nipy = topik.nipy_fk_nipy
+            JOIN topik_bidang ON topik_bidang.id = topik.topik_bidang_fk_id
+            LEFT OUTER JOIN ambil_topik_tugas_akhir ambil ON ambil.topik_tugas_akhir_id = topik.id
+            GROUP BY topik.id
+            ORDER BY dosen.nipy = '.$nipy.' DESC');
+
+        return view('all-topik')->with("allTopikTA", $allTopikTA);
+    }
+
+    public function store(Request $request)
+    {
         $request->validate([
-            'topik_bidang'=>'required',
-            'judul'=>'required|min:5',
+            'topik_bidang' => 'required',
+            'judul' => 'required|min:5',
             'deskripsi' => 'required|min:5',
         ]);
-        
-        if($request){
+
+        if ($request) {
             $store = new TopikTugasAkhir;
             $store->nipy_fk_nipy = Session::get('nipy');
             $store->topik_bidang_fk_id = $request->topik_bidang;
@@ -30,14 +50,8 @@ class TopikController extends Controller
             $store->nim_terpilih_fk;
             $store->save();
 
-            return back()->with('success', 'Topik tugas akhir berhasil di tambahkan');
-
-            /*
-            setelah halaman view all topik tugas akhir jadi, bisa langsung
-            return redirect(#halaman view)->with('success', ''Message');
-            */
-        }
-        else{
+            return redirect('/Topik/All')->with('success', 'Topik tugas akhir berhasil di tambahkan');;
+        } else {
             return redirect('/Topik/Add');
         }
     }
