51fc9fa83f216ec0abb1cdc33728339d54839475
Aqsalya Indira <62454064+aqslindr@users.noreply.github.com>
2021-05-31 18:53:26 +0700

    THESIS (KERUCUT)_TUGAS PERTEMUAN 8_1900018185_AQSALYA INDIRA_KELAS D

---
+++ BangunRuang.php
@@ -0,0 +1,17 @@
+<?php
+# app/BangunRuang.php
+
+namespace App;
+
+class BangunRuang
+{
+    /**
+     * Menghitung nilai pangkat dua
+     * Rumus: nilai x nilai
+     */
+    function kuadrat($nilai)
+    {
+        $PANGKAT_DUA = 2;
+        return pow($nilai, $PANGKAT_DUA);
+    }   
+}
