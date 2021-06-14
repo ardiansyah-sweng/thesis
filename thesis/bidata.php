13b4222b09c19ed13ff00f1a2055d9e8df7cd6d6
omaulidya <omaulidya@gmail.com>
2021-06-14 19:45:17 +0700

    ochamaulidya_e_1900018231 (pertemuan 10)

---
+++ thesis/bidata.php
@@ -0,0 +1,61 @@
+<?php
+class User
+{
+    public $nama;
+    public $email;
+    public $dob;
+
+    public function __construct($data)
+    {
+        $this->nama = $data['nama'];
+        $this->email = $data['email'];
+        $this->dob = $data['dob'];
+    }
+}
+
+class UserRequest
+{
+    protected static $rules = [
+        'nama' => 'string',
+        'email' => 'string',
+        'dob' => 'string'
+    ];
+
+    public static function validate($data){
+        foreach (static::$rules as $property => $type){
+            if (gettype($data[$property]) != $type){
+                throw new \Exception("User property {$property} must be of type {$type}" );
+            }
+        }
+    }
+}
+
+class Json{
+    public static function from ($data){
+        return json_encode($data);
+    }
+}
+
+class Age{
+    public static function now($data){
+        $dob = new DateTime($data['dob']);
+        $today = new Datetime(date('d.m.y'));
+        return [
+            'year' => $today->diff($dob)->y,
+            'month' => $today->diff($dob)->m,
+            'day' => $today->diff($dob)->d,
+        ];
+    }
+}
+
+$data = [
+    'nama' => 'OchaMaulidya', 
+    'email' => 'ocha1900018231@webmail.uad.ac.id',
+    'dob' => '28.5.2001'
+];
+
+UserRequest::validate($data);
+$user = new User($data);
+print_r(Json::from($user));
+echo '<br>';
+print_r(Age::now($data));
