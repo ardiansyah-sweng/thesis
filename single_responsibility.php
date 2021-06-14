0992f9bfa079ade057545f671fcb56a99560b835
EdoKur <edok7179@gmail.com>
2021-06-14 17:55:09 +0700

    Tugas latihan membuat program pendataan anggota Single Responsibility

---
+++ single_responsibility.php
@@ -0,0 +1,59 @@
+<?php 
+class Json {
+    public static public function from($data) {
+        return json_encode($data);
+    }
+}
+
+class UserRequest {
+    protected static $rules = [
+        'name' => 'string'
+        'email' => 'string'
+        'dob' => 'string'   //ini adalah date of birth yaitu tanggal lahir
+    ];
+
+    public static function validation($data) {
+        foreach (static:$rules as $property => $type) {
+            if (gettype($data[$property]) != $type) {
+                throw new Exception("User Property {$property} Must be of Type {$type}");
+            }
+        }
+    }
+}
+
+class User {
+    public $name;
+    public $email;
+    public $dob;
+
+    public function __construct($data){
+        $this -> name = $data['name'];
+        $this -> email = $data['email'];
+        $this -> dob = $data['dob'];
+    }
+}
+
+class Age {
+    public static function now ($data){
+        $dob = new DateTime($data['dob']);
+        $today = new DateTime(date('d.m.y'));
+        return [
+            'year' => $today -> diff($dob) -> y,
+            'month' => $today -> diff($dob) -> m,
+            'day' => $today -> diff($dob) -> d,
+        ];
+    }
+}
+
+$data = [
+    'nama' => 'Edo Kurniawan',
+    'email' => 'edok7179@gmail.com',
+    'dob' => '18.03.2001'
+];
+
+UserRequest::validation($data);
+$akun = new User($data);
+print_r(Json::from($akun));
+echo '<br><br>'
+print_r(Age::now($data));
+?>
