9138c8ed92061e6b4a4bcfbdaad25e3af2095175
omaulidya <omaulidya@gmail.com>
2021-07-14 22:28:20 +0800

    perbaikan pertemuan 11-12

--- rruang.php
+++ rruang.php
@@ -65,23 +65,55 @@ class kalkulatorBangunRuangFactory
     {
     if($tipe==='luaspersegipanjang')
     {
-        return Lpersegipanjang();
+        $data1 = new LPersegi();
+        $data1 -> panjang = $satuan['panjang'] ;
+        $data1 -> lebar = $satuan['lebar'] ;
+
+        echo "panjangnya adalah : " , $satuan['panjang'],"<br>";
+        echo "lebarnya adalah : " , $satuan['lebar'],"<br>";
+        echo "Hasil volumenya adalah : ";
+        return $data1;
     }
     if($tipe==='volumebola')
     {
-        return Vbola();
+        $data2 = new Volbola();
+        $data2 -> jari = $satuan['jari'] ;
+        $data2 -> phi = $satuan['phi'] ;
+
+        echo "Jari - jarinya adalah : " , $satuan['jari'],"<br>";
+        echo "Hasil volumenya adalah : = ";
+        return $data2;
     }
     if($tipe==='volumekerucut')
     {
-        return Vkerucut();
+        $data3 = new Volkerucut();
+        $data3 -> tinggi = $satuan['tinggi'] ;
+        $data3 -> jari = $satuan['jari'] ;
+        $data3 -> phi = $satuan['phi'] ;
+
+        echo "Jari- jarinya adalah : " , $satuan['jari'],"<br>";
+        echo "tingginya adalah : " , $satuan['tinggi'],"<br>";
+        echo "Hasil volumenya adalah : ";
+        return $data3;
     }
     if($tipe==='volumekubus')
     {
-        return Vkubus();
+        $data4 = new Volkubus();
+        $data4 -> rusuk = $satuan['rusuk'] ;
+
+        echo "Rusuknya adalah : " , $satuan['rusuk'],"<br>";
+        echo "Hasil volumenya adalah : ";
+        return $data4;
     }
     if($tipe==='kelilinglingkaran')
     {
-        return Klingkaran();
+        $data1 = new Kelingkaran();
+            $data5 -> jari = $satuan['jari'] ;
+            $data5 -> phi = $satuan['phi'] ;
+
+            echo "Jari - jarinya adalah : " , $satuan['jari'],"<br>";
+            echo "Hasil volumenya adalah : ";
+            return $data5;
     }
     throw new Exception("Coba lagi");
     }
