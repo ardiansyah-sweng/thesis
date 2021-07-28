f1ab24479d84c624a5b3e65896541cf89635cca0
sendyapriatna <sendyapriatna001@gmail.com>
2021-07-28 10:48:41 +0700

    Tugas Single Responsiblity

--- single_responsiblity.php
+++ single_responsiblity.php
@@ -1,59 +1,61 @@
-<?php 
-class Json {
-    public static public function from($data) {
-        return json_encode($data);
+<?php
+class User
+{
+    public $nama;
+    public $email;
+    public $dob;
+
+    public function __construct($data)
+    {
+        $this->nama = $data['nama'];
+        $this->email = $data['email'];
+        $this->dob = $data['dob'];
     }
 }
 
-class UserRequest {
+class UserRequest
+{
     protected static $rules = [
-        'name' => 'string'
-        'email' => 'string'
-        'dob' => 'string'   //ini adalah date of birth yaitu tanggal lahir
+        'nama' => 'string',
+        'email' => 'string',
+        'dob' => 'string'
     ];
 
-    public static function validation($data) {
-        foreach (static:$rules as $property => $type) {
-            if (gettype($data[$property]) != $type) {
-                throw new Exception("User Property {$property} Must be of Type {$type}");
+    public static function validate($data){
+        foreach (static::$rules as $property => $type){
+            if (gettype($data[$property]) != $type){
+                throw new \Exception("User property {$property} must be of type {$type}" );
             }
         }
     }
 }
 
-class User {
-    public $name;
-    public $email;
-    public $dob;
-
-    public function __construct($data){
-        $this -> name = $data['name'];
-        $this -> email = $data['email'];
-        $this -> dob = $data['dob'];
+class Json{
+    public static function from ($data){
+        return json_encode($data);
     }
 }
 
-class Age {
-    public static function now ($data){
+class Age{
+    public static function now($data){
         $dob = new DateTime($data['dob']);
-        $today = new DateTime(date('d.m.y'));
+        $today = new Datetime(date('d.m.y'));
         return [
-            'year' => $today -> diff($dob) -> y,
-            'month' => $today -> diff($dob) -> m,
-            'day' => $today -> diff($dob) -> d,
+            'Tahun' => $today->diff($dob)->y,
+            'Bulan' => $today->diff($dob)->m,
+            'Hari' => $today->diff($dob)->d,
         ];
     }
 }
 
 $data = [
-    'nama' => 'Sendy Apriatna',
-    'email' => 'sendyapriatna@yahoo.co.id',
+    'nama' => 'Sendy_Apriatna', 
+    'email' => 'sendy19000285@webmail.uad.ac.id',
     'dob' => '20.04.2001'
 ];
 
-UserRequest::validation($data);
-$akun = new User($data);
-print_r(Json::from($akun));
-echo '<br><br>'
-print_r(Age::now($data));
-?>
+UserRequest::validate($data);
+$user = new User($data);
+print_r(Json::from($user));
+echo '<br>';
+echo "Usia : " . Age::now($data)['Tahun'] . " Tahun " . Age::now($data)['Bulan'] . " Bulan " . Age::now($data)['Hari'] . " Hari ";
